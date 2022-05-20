1fb1badf855798f31b6ce19ebe6e7307529df1b6
5pamm3r <cmassarino.dev@gmail.com>
2022-05-20 16:18:25 -0300

    cambio de switch por obj

--- scripts/changeDayHours.js
+++ scripts/changeDayHours.js
@@ -1,46 +1,22 @@
 export const changeDayHours = (state, el, background) => {
-    const weatherMain = el.weather[0].main
-    console.log(weatherMain)
-    if (state === true) {
-        switch (weatherMain) {
-            case 'Clear':
-                background('url("https://i.postimg.cc/NfS4r0CP/soleado.jpg")')
-                break;
-            case 'Clouds' || 'Mist':
-                background('url("https://i.postimg.cc/DwCbbpzf/parcial.jpg")')
-                break;
-            case 'Mist':
-                background('url("https://i.postimg.cc/DwCbbpzf/parcial.jpg")')
-                break;
-            case 'Rain':
-                background('url("https://i.postimg.cc/fWxtvRbr/lluvioso.jpg")')
-                break;
-            case 'Thunderstorm':
-                background('url("https://i.postimg.cc/fWxtvRbr/lluvioso.jpg")')
-                break;
-            default:
-                console.log('default')
-                break;
-        }
-    } else {
-        switch (weatherMain) {
-            case 'Clear':
-                background('url("https://i.postimg.cc/NfS4r0CP/noche-despejado.jpg")')
-                break;
-            case 'Clouds':
-                background('url("https://i.postimg.cc/DwCbbpzf/noche-parcial.jpg")')    
-                break
-            case 'Mist':
-                background('url("https://i.postimg.cc/DwCbbpzf/noche-parcial.jpg")')
-                break;
-            case 'Rain': 
-                background('url("https://i.postimg.cc/fWxtvRbr/noche-lluvioso.jpg")')
-                break
-            case 'Thunderstorm':
-                background('url("https://i.postimg.cc/fWxtvRbr/noche-lluvioso.jpg")')
-                break;
-            default:
-                break;
-        }
-    }
-}
+  const weatherMain = el.weather[0].main;
+
+  const backObj = {
+    true: {
+      Clear: 'url("https://i.postimg.cc/NfS4r0CP/soleado.jpg")',
+      Clouds: 'url("https://i.postimg.cc/DwCbbpzf/parcial.jpg")',
+      Mist: 'url("https://i.postimg.cc/DwCbbpzf/parcial.jpg")',
+      Rain: 'url("https://i.postimg.cc/fWxtvRbr/lluvioso.jpg")',
+      Thunderstorm: 'url("https://i.postimg.cc/fWxtvRbr/lluvioso.jpg")',
+    },
+    false: {
+      Clear: 'url("https://i.postimg.cc/NfS4r0CP/noche-despejado.jpg")',
+      Clouds: 'url("https://i.postimg.cc/DwCbbpzf/noche-parcial.jpg")',
+      Mist: 'url("https://i.postimg.cc/DwCbbpzf/noche-parcial.jpg")',
+      Rain: 'url("https://i.postimg.cc/fWxtvRbr/noche-lluvioso.jpg")',
+      Thunderstorm: 'url("https://i.postimg.cc/fWxtvRbr/noche-lluvioso.jpg")',
+    },
+  };
+
+  background(backObj[state][weatherMain]);
+};
