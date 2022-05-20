99113c091a90d4ac4d5f91691c4b721d32bf54f1
5pamm3r <cmassarino.dev@gmail.com>
2022-05-20 16:18:53 -0300

    indent

--- scripts/design.js
+++ scripts/design.js
@@ -1,34 +1,36 @@
-import {changeDayHours} from './changeDayHours.js'
-
+import { changeDayHours } from "./changeDayHours.js";
 
 const createInputs = (data) => {
-    const date = new Date(data.dt*1000).toLocaleString('es-ES', {
-        dateStyle: 'medium'
-    })
-    document.querySelector('#input__temp').value = `${Math.floor(data.main.temp)}°`
-    document.querySelector('#input__date').value = date
-    document.querySelector('#input__city').value = `${data.name}, ${data.sys.country}`
-}
+  const date = new Date(data.dt * 1000).toLocaleString("es-ES", {
+    dateStyle: "medium",
+  });
+  document.querySelector("#input__temp").value = `${Math.floor(
+    data.main.temp
+  )}°`;
+  document.querySelector("#input__date").value = date;
+  document.querySelector(
+    "#input__city"
+  ).value = `${data.name}, ${data.sys.country}`;
+};
 
 const background = (img) => {
-    document.querySelector('#container').style.backgroundImage = img
-    document.querySelector('#container').style.backgroundSize = 'cover'
-    document.querySelector('#container').style.backgroundPosition = 'center'
-}
+  document.querySelector("#container").style.backgroundImage = img;
+  document.querySelector("#container").style.backgroundSize = "cover";
+  document.querySelector("#container").style.backgroundPosition = "center";
+};
 
 const changeBackground = (el, background) => {
-    const dayHour = new Date(el.dt*1000).getHours()  
-    if (dayHour >= 6 && dayHour < 19) {
-        changeDayHours(true, el, background)
-    } else {
-        changeDayHours(false, el, background)
-    }
-    
-}
+  const dayHour = new Date(el.dt * 1000).getHours();
+  if (dayHour >= 6 && dayHour < 19) {
+    changeDayHours(true, el, background);
+  } else {
+    changeDayHours(false, el, background);
+  }
+};
 
 const iconTemp = (codeIcon) => {
-    const img = document.querySelector('#icon__temp')
-    img.src = `https://openweathermap.org/img/wn/${codeIcon}.png`
-}
+  const img = document.querySelector("#icon__temp");
+  img.src = `https://openweathermap.org/img/wn/${codeIcon}.png`;
+};
 
-export { createInputs, background, changeBackground, iconTemp }
+export { createInputs, background, changeBackground, iconTemp };
