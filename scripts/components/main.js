abc251ea221a0ae1c55f8eec40a63d060a1a43c8
5pamm3r <cmassarino.dev@gmail.com>
2022-10-31 18:43:56 -0300

    Back btn and change style with click

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -1,12 +1,19 @@
 import { nextDays } from "./nextDays.js";
 import { nextHours } from "./nextHours.js";
-import { todayContainer, fragmentTodayContainer } from './todayContainer.js'
+import { todayContainer, fragmentTodayContainer } from "./todayContainer.js";
 
 const main = (data) => {
-  document.querySelector('#location').textContent = `${data.location.region}, ${data.location.country}`;
-  document.querySelector('#temperature').textContent = `${Math.floor(data.current.temp_c)}°`;
-  document.querySelector('#description').textContent = data.current.condition.text;
-  document.querySelector('#day').textContent = new Date(data.location.localtime_epoch * 1000).toLocaleString('en-EN',{dateStyle: 'medium'});
+  document.querySelector(
+    "#location"
+  ).textContent = `${data.location.region}, ${data.location.country}`;
+  document.querySelector("#temperature").textContent = Math.floor(
+    data.current.temp_c
+  );
+  document.querySelector("#description").textContent =
+    data.current.condition.text;
+  document.querySelector("#day").textContent = new Date(
+    data.location.localtime_epoch * 1000
+  ).toLocaleString("en-EN", { dateStyle: "medium" });
   todayContainer(
     "https://img.icons8.com/office/30/undefined/wind--v1.png",
     `${data.current.wind_kph} km/h`,
@@ -22,12 +29,26 @@ const main = (data) => {
     data.current.precip_mm,
     "Precip. mm"
   );
-  document.querySelector('#InfoTodayContainer').append(fragmentTodayContainer);
-  
-  nextHours(data.location.region, data.location.country)
+  document.querySelector("#InfoTodayContainer").append(fragmentTodayContainer);
+  nextHours(data.location.region, data.location.country);
+  document
+    .querySelector("#nextDays")
+    .addEventListener("click", () => nextDays(data));
 
+  document.querySelector("#backBtn").addEventListener("click", () => {
+    while (document.querySelector("#nextDaysContainer").firstElementChild) {
+      document.querySelector("#nextDaysContainer").firstElementChild.remove();
+    }
+    document.querySelector(".footerSubtitle__container").style.display = "flex";
+    document.querySelector("#nextHoursContainer").style.display = "flex";
+    document.querySelector("#nextDays").style.display = "inline-block";
+    document.querySelector("#backBtn").style.display = "none";
+  });
 
-  document.querySelector('#nextDays').addEventListener('click', ()=>nextDays(data))
+  document.querySelector(".location").addEventListener("click", () => {
+    document.querySelector(".location").style.display = "none";
+    document.querySelector(".search__form").style.display = "flex";
+  });
 };
 
 export { main };
