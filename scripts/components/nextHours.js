1c72224b16ca94c14d7da4761ff51842dad099c5
5pamm3r <cmassarino.dev@gmail.com>
2022-10-31 18:48:40 -0300

    Add one more item for container

--- scripts/components/nextHours.js
+++ scripts/components/nextHours.js
@@ -1,3 +1,4 @@
+import { API_KEY } from "./apiKey.js";
 import { nextHoursContainer, fragmentNextHours } from "./nextHoursContainer.js";
 
 export const nextHours = async (city, country) => {
@@ -6,7 +7,7 @@ export const nextHours = async (city, country) => {
       `https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=1&location=${city}%2C${country}&contentType=json&unitGroup=metric&shortColumnNames=true`,
       {
         headers: {
-          'X-RapidAPI-Key': '03180f7dcemsh85b598425ef8801p16815ajsnb73ba7cd518c',
+          'X-RapidAPI-Key': API_KEY,
           'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
         }
       }
@@ -15,6 +16,7 @@ export const nextHours = async (city, country) => {
     nextHoursContainer('https://img.icons8.com/color/48/undefined/rain--v1.png', Math.floor(data.locations[`${city},${country}`].values[3].temp), new Date(data.locations[`${city},${country}`].values[3].datetime).getHours())
     nextHoursContainer('https://img.icons8.com/color/48/undefined/rain--v1.png', Math.floor(data.locations[`${city},${country}`].values[6].temp), new Date(data.locations[`${city},${country}`].values[6].datetime).getHours())
     nextHoursContainer('https://img.icons8.com/color/48/undefined/rain--v1.png', Math.floor(data.locations[`${city},${country}`].values[9].temp), new Date(data.locations[`${city},${country}`].values[9].datetime).getHours())
+    nextHoursContainer('https://img.icons8.com/color/48/undefined/rain--v1.png', Math.floor(data.locations[`${city},${country}`].values[12].temp), new Date(data.locations[`${city},${country}`].values[12].datetime).getHours())
     document.querySelector('#nextHoursContainer').append(fragmentNextHours)
   } catch (error) {
     console.error(error)
