558ca3474addb957c7ea885fade9574103582dfd
5pamm3r <cmassarino.dev@gmail.com>
2022-12-26 20:49:12 -0300

    Add diferents weather forecast background

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,25 @@
 import { main } from "./components/main.js";
+import rain from "./components/animations/rain.js";
+import thunderstorm from "./components/animations/thunderstorm.js";
+import sunny from "./components/animations/sunny.js";
+import cloudy from "./components/animations/cloudy.js";
 const getWeatherData = async (city) => {
   try {
-    const res = await fetch(
-      `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`,
-      {
-        headers: {
-          "X-RapidAPI-Key":
-            '03180f7dcemsh85b598425ef8801p16815ajsnb73ba7cd518c',
-          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-        },
-      }
-    );
-    const data = await res.json();
+    // const res = await fetch(
+    //   `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`,
+    //   {
+    //     headers: {
+    //       "X-RapidAPI-Key":
+    //         '03180f7dcemsh85b598425ef8801p16815ajsnb73ba7cd518c',
+    //       "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    //     },
+    //   }
+    // );
+    // const data = await res.json();
 
-    main(data);
+    // main(data);
+    main()
+    // rain()
   } catch (err) {
     console.error(err);
   }
@@ -29,4 +35,17 @@ document.body.addEventListener("submit", (e) => {
 
 window.onload = () => {
   getWeatherData("Salto");
+  const classThunder = document.getElementById('weatherIcon').classList.contains('thunderstorm');
+  const classRainn = document.getElementById('weatherIcon').classList.contains('rain');
+  const classSunny = document.getElementById('weatherIcon').classList.contains('sunny');
+  const classCloudy = document.getElementById('weatherIcon').classList.contains('cloudy');
+  if(!!classRainn) {
+    rain()
+  } else if(!!classThunder) {
+    thunderstorm()
+  } else if(!!classSunny) {
+    sunny()
+  } else if(!!classCloudy) {
+    cloudy()
+  }
 };
