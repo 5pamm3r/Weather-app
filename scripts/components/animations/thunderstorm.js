715d4c774364f83e533dd84ceee850500c6f810e
5pamm3r <cmassarino.dev@gmail.com>
2022-12-26 20:54:36 -0300

    Add thunderstorm effect

---
+++ scripts/components/animations/thunderstorm.js
@@ -0,0 +1,20 @@
+// 88 88 110
+import rain from "./rain.js";
+const container = document.getElementById("backAnimate");
+
+const thunderstorm = () => {
+  const img = document.createElement("img");
+  const img2 = document.createElement('img');
+  img.src = "../../public/img/images/lightning-2.png";
+  img2.src = '../../public/img/images/lightning.png'
+  document.getElementById('mainContainer').classList.remove('rain', 'sunny', 'cloudy')
+  document.getElementById('mainContainer').classList.add('thunder')
+  document.getElementById('vapour').style.opacity = 1;
+  container.classList.remove('back__rain','back__sunny','back__cloudy');
+  container.classList.add('back__thunderstorm')
+  container.append(img, img2);
+
+
+  rain("0.4s");
+};
+export default thunderstorm;
