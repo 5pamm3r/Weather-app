23725f25ad16ef9cc49b1a72be72b025ab8d7588
5pamm3r <cmassarino.dev@gmail.com>
2022-12-26 20:53:17 -0300

    Add cloudy effect

---
+++ scripts/components/animations/cloudy.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('backAnimate');
+
+const cloudy = () => {
+  document.getElementById('vapour').classList.remove('block');
+  const cloud = document.createElement('img');
+  const cloud2 = document.createElement('img');
+
+  cloud.src = '../../public/img/images/cloud.png';
+  cloud2.src = '../../public/img/images/cloud.png';
+  container.classList.remove('back__thunderstorm','back__sunny','back__rain');
+  container.classList.add('back__cloudy');
+
+
+  container.append(cloud, cloud2)
+}
+export default cloudy;
