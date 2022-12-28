1d4c49fe90894c32a396d479146857ef981a1cbd
5pamm3r <cmassarino.dev@gmail.com>
2022-12-28 09:42:25 -0300

    Change classList

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -5,47 +5,63 @@ import { todayContainer, fragmentTodayContainer } from "./todayContainer.js";
 const main = (data) => {
   document.querySelector(
     "#location"
-  ).textContent = `${data.location.region}, ${data.location.country}`;
-  document.querySelector("#temperature").textContent = Math.floor(
+  ).textContent = 'Salto, Uy'/*`${data.location.region}, ${data.location.country}`*/;
+  document.querySelector("#temperature").textContent = 25/*Math.floor(
     data.current.temp_c
-  );
+  )*/;
   document.querySelector("#description").textContent =
-    data.current.condition.text;
-  document.querySelector("#day").textContent = new Date(
+    'Nublado'/*data.current.condition.text*/;
+  document.querySelector("#day").textContent = 'Martes 5 diciembre'/*new Date(
     data.location.localtime_epoch * 1000
-  ).toLocaleString("en-EN", { dateStyle: "medium" });
+  ).toLocaleString("en-EN", { dateStyle: "medium" })*/;
   todayContainer(
-    "https://img.icons8.com/office/30/undefined/wind--v1.png",
-    `${data.current.wind_kph} km/h`,
+    "../../public/img/icons/wind.png",
+    '16'/*`${data.current.wind_kph} km/h`*/,
     "Wind speed"
   );
   todayContainer(
-    "https://img.icons8.com/external-justicon-flat-justicon/64/undefined/external-humidity-weather-justicon-flat-justicon.png",
-    `${data.current.humidity} %`,
+    '../../public/img/icons/humidity.png',
+    '13'/*`${data.current.humidity} %`*/,
     "Humidity"
   );
   todayContainer(
-    "https://img.icons8.com/color/48/undefined/rain--v1.png",
-    data.current.precip_mm,
+    '../../public/img/icons/rain-v.png',
+    '12'/*data.current.precip_mm*/,
     "Precip. mm"
   );
-  document.querySelector("#InfoTodayContainer").append(fragmentTodayContainer);
-  nextHours(data.location.region, data.location.country);
-  document
-    .querySelector("#nextDays")
-    .addEventListener("click", () => nextDays(data));
+  document.querySelector("#infoTodayContainer").append(fragmentTodayContainer);
+  // nextHours(data.location.region, data.location.country);
+  nextHours('Salto','Uy')
+  if(window.innerWidth > '800') {
+    // nextDays(data)
+    nextDays()
+  } else {
+    document
+    .querySelector("#nextDaysBtn")
+    // .addEventListener("click", () => nextDays(data));
+    .addEventListener("click", () => nextDays());
+  }
 
   document.querySelector("#backBtn").addEventListener("click", () => {
     while (document.querySelector("#nextDaysContainer").firstElementChild) {
       document.querySelector("#nextDaysContainer").firstElementChild.remove();
     }
-    document.querySelector(".footerSubtitle__container").style.display = "flex";
-    document.querySelector("#nextHoursContainer").style.display = "flex";
-    document.querySelector("#nextDays").style.display = "inline-block";
-    document.querySelector("#backBtn").style.display = "none";
-  });
+    document.querySelector(".footerSubtitle__container").classList.remove('none');
+    document.querySelector("#nextHoursContainer").classList.remove('none');
+    document.querySelector("#nextDaysBtn").classList.remove('none');
+    document.querySelector("#backBtn").classList.remove('block');
 
+    document.querySelector("#backBtn").classList.add('none');
+    document.querySelector(".footerSubtitle__container").classList.add('flex');
+    document.querySelector("#nextHoursContainer").classList.add('flex');
+    document.querySelector("#nextDaysBtn").classList.add('block');
+  });
   document.querySelector(".location").addEventListener("click", () => {
+    setTimeout(() => {
+      
+      document.querySelector(".location").style.display = "block";
+      document.querySelector(".search__form").style.display = "none";
+    }, 5000);
     document.querySelector(".location").style.display = "none";
     document.querySelector(".search__form").style.display = "flex";
   });
