2099a882fe14a5c0912276b5b9002a721608e43e
5pamm3r <cmassarino.dev@gmail.com>
2023-02-22 11:36:21 -0300

    Fix word

--- scripts/components/animations/sunny.js
+++ scripts/components/animations/sunny.js
@@ -1,14 +1,13 @@
-// 0 186 224
 const container = document.getElementById('backAnimate');
 const sunny = () => {
   document.getElementById('vapour').classList.remove('block');
-  const brightning = document.createElement('div')
-  brightning.classList.add('brightning')
+  const brightening = document.createElement('div')
+  brightening.classList.add('brightening')
   container.classList.remove('back__thunderstorm', 'back__cloudy', 'back__rain');
   container.classList.add('back__sunny')
   document.getElementById('weatherIconContainer').style.zIndex = 1;
   document.getElementById('mainContainer').classList.remove('cloudy','rain','thunderstorm');
   document.getElementById('mainContainer').classList.add('sunny');
-  container.append(brightning)
+  container.append(brightening)
 }
 export default sunny;
