64848c94f73292fc742599b3c2e5534061dcb935
5pamm3r <cmassarino.dev@gmail.com>
2023-03-06 17:00:27 -0300

    Change api

--- scripts/components/nextHours.js
+++ scripts/components/nextHours.js
@@ -2,21 +2,41 @@ import { nextHoursContainer, fragmentNextHours } from "./nextHoursContainer.js";
 
 export const nextHours = async (city, country) => {
   try {
-    const res = await fetch(
-      `https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=1&location=${city}%2C${country}&contentType=json&unitGroup=metric&shortColumnNames=true`,
-      {
-        headers: {
-          'X-RapidAPI-Key': '03180f7dcemsh85b598425ef8801p16815ajsnb73ba7cd518c',
-          'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
-        }
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'ba2212a8a8mshc21e4bba59bebd4p1d5811jsn33fe8b053950',
+        'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
       }
-    );
-    const data = await res.json();
-    nextHoursContainer('https://5pamm3r.github.io/umbrella/public/img/images/sunny.png', Math.floor(data.locations[`${city},${country}`].values[3].temp), new Date(data.locations[`${city},${country}`].values[3].datetime).getHours())
-    nextHoursContainer('https://5pamm3r.github.io/umbrella/public/img/images/cloudy.png', Math.floor(data.locations[`${city},${country}`].values[6].temp), new Date(data.locations[`${city},${country}`].values[6].datetime).getHours())
-    nextHoursContainer('https://5pamm3r.github.io/umbrella/public/img/images/cloudy.png', Math.floor(data.locations[`${city},${country}`].values[9].temp), new Date(data.locations[`${city},${country}`].values[9].datetime).getHours())
-    nextHoursContainer('https://5pamm3r.github.io/umbrella/public/img/images/cloudy.png', Math.floor(data.locations[`${city},${country}`].values[12].temp), new Date(data.locations[`${city},${country}`].values[12].datetime).getHours())
-    document.querySelector('#nextHoursContainer').append(fragmentNextHours)
+    };
+    const getCoord = async (city, country) => {
+      try{
+    
+        const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=5&appid=8d9eeb8926ac32c224c151d4599f79b6`)
+        const data = await res.json()
+        getWeather(data[0].lat, data[0].lon)
+      }
+      catch(error) {
+        console.error(error)
+      }
+    }
+    const getWeather = async (lat, lon) => {
+      try{
+    
+        const res = await fetch(`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=${lat}&lon=${lon}&units=metric&lang=en`, options)
+        const data = await res.json()
+        console.log(data)
+        nextHoursContainer(`https://5pamm3r.github.io/umbrella/public/img/icons/weather/${data.data[0].weather.icon}.png`, Math.floor(data.data[0].temp), new Date(data.data[0].ts * 1000).getHours())
+        nextHoursContainer(`https://5pamm3r.github.io/umbrella/public/img/icons/weather/${data.data[1].weather.icon}.png`, Math.floor(data.data[1].temp), new Date(data.data[1].ts * 1000).getHours())
+        nextHoursContainer(`https://5pamm3r.github.io/umbrella/public/img/icons/weather/${data.data[2].weather.icon}.png`, Math.floor(data.data[2].temp), new Date(data.data[2].ts * 1000).getHours())
+        nextHoursContainer(`https://5pamm3r.github.io/umbrella/public/img/icons/weather/${data.data[3].weather.icon}.png`, Math.floor(data.data[3].temp), new Date(data.data[3].ts * 1000).getHours())
+        document.querySelector('#nextHoursContainer').append(fragmentNextHours)
+      } catch(err){
+        console.error(err)
+      }
+    }
+    
+    getCoord(city, country)
   } catch (error) {
     console.error(error)
   }
